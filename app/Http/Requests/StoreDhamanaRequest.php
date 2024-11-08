907b9be4e633e4b90247f21ed14454a4becfaaaf
Lucas John <lucasjohn414@gmail.com>
2024-11-08 10:15:58 +0300

    Update StoreDhamanaRequest.php

--- app/Http/Requests/StoreDhamanaRequest.php
+++ app/Http/Requests/StoreDhamanaRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoredhamanaRequest extends FormRequest
+class StoreDhamanaRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
